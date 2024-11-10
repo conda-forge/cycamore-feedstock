fbed255868dc9a967433c298ea05d40e44a75fc2
bennibbelink <79653949+bennibbelink@users.noreply.github.com>
2024-11-10 16:40:08 -0500

    add support for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$(uname)" == "Darwin" ]; then
-  # other
-  libext=".dylib"
-  export LDFLAGS="-rpath ${PREFIX}/lib ${LDFLAGS}"
-  export LINKFLAGS="${LDFLAGS}"
-  skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
-else
-  libext=".so"
-  skiprpath=""
-fi
-
 # Install Cycamore
 #export VERBOSE=1
 ${PYTHON} install.py \
   --prefix=${PREFIX} \
+  ${CMAKE_ARGS} \
   -DCMAKE_C_COMPILER=${GCC} \
   -DCMAKE_CXX_COMPILER=${CXX} \
   --allow-milps \
